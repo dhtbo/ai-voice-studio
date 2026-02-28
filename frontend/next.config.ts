7807bbf6f7a8d154ee8f1e25e57c773ea6a3f5ef
董红涛 <dhtbo@live.cn>
2026-02-28 15:26:41 +0800

    ok

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,7 @@
 const nextConfig: any = {
   experimental: {
     allowedDevOrigins: [
+      'https://ai-voice-studio-kohl.vercel.app/',
       'https://overmoralizingly-nonconsular-jacqualine.ngrok-free.dev',
       'http://localhost:3000',
     ],
